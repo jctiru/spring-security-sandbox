7b43350735a3c00fa1ab41ed50d6d6f3ab56c91a
jctiru <jonathanchristiantiru@gmail.com>
2019-09-14 18:38:53 +0800

    Configure hibernate and transaction manager

--- spring-security-sandbox/src/main/java/io/jctiru/springsecuritysandbox/config/AppConfig.java
+++ spring-security-sandbox/src/main/java/io/jctiru/springsecuritysandbox/config/AppConfig.java
@@ -1,16 +1,20 @@
 package io.jctiru.springsecuritysandbox.config;
 
 import java.beans.PropertyVetoException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -63,6 +67,42 @@ public class AppConfig {
 		securityDataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdleTime")));
 
 		return securityDataSource;
+	}
+
+	private Properties getHibernateProperties() {
+
+		// Set hibernate properties
+		Properties props = new Properties();
+
+		props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+
+		return props;
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+
+		// Create session factory
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+		// Set the properties
+		sessionFactory.setDataSource(securityDataSource());
+		sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+		sessionFactory.setHibernateProperties(getHibernateProperties());
+
+		return sessionFactory;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+
+		// Setup transaction manager based on session factory
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+
+		return txManager;
 	}
 
 }
