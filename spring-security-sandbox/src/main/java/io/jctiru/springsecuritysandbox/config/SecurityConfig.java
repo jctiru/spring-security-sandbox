204c7f58d8863f9a9e7355ee981807d305c3e4ba
jctiru <jonathanchristiantiru@gmail.com>
2019-09-15 22:33:21 +0800

    Change authentication in security configuration

--- spring-security-sandbox/src/main/java/io/jctiru/springsecuritysandbox/config/SecurityConfig.java
+++ spring-security-sandbox/src/main/java/io/jctiru/springsecuritysandbox/config/SecurityConfig.java
@@ -1,43 +1,35 @@
 package io.jctiru.springsecuritysandbox.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
-	private DataSource securityDataSource;
+	private UserDetailsService userService;
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(securityDataSource);
-		
-		// Add users for in-memory auth
-		/*
-		 * UserBuilder users = User.withDefaultPasswordEncoder();
-		 * auth.inMemoryAuthentication()
-		 * .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-		 * .withUser(users.username("mary").password("test123").roles("EMPLOYEE",
-		 * "MANAGER"))
-		 * .withUser(users.username("susan").password("test123").roles("EMPLOYEE",
-		 * "ADMIN"));
-		 */
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-//				.anyRequest().authenticated()
 				.antMatchers("/").hasRole("EMPLOYEE")
 				.antMatchers("/leaders/**").hasRole("MANAGER")
 				.antMatchers("/systems/**").hasRole("ADMIN")
